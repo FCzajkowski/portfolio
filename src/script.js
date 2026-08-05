8e77a81c459e659645cd82dda9a34364a7952292
Franciszek <franiu325@gmail.com>
2026-08-05 22:30:45 +0200

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,17 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const img7 = document.getElementById("img6");
+  const img2 = document.getElementById("img2");
+  const img5 = document.getElementById("img5");
+
+  img7?.addEventListener("click", () => {
+    window.open("https://fczajkowski.github.io/TaurusEnergyDrink/", "_blank");
+  });
+
+  img2?.addEventListener("click", () => {
+    window.open("https://www.instagram.com/p/DagI-FejOSH/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==", "_blank");
+  });
+
+  img5?.addEventListener("click", () => {
+    window.open("https://github.com/FCzajkowski/Hajime-Framework", "_blank");
+  });
+});
